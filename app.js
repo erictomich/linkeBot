16edb90a9c576ddc705ebb70ef1fcc5babc34d1a
Eric <erictomich@gmail.com>
2022-03-16 21:44:31 -0300

    alteracoes

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 import { runBot } from './back/main.js'
 import express from 'express';
+import { readFileSync } from 'fs';
+
+const output = readFileSync(new URL('./back/data.json', import.meta.url));
 
 const app = express();
 const port = 3000
 
 app.use(express.static('front'));
+
+app.get('/datajson', (req, res) => {
+  res.send(output.toString());
+})
 
 runBot();
 
